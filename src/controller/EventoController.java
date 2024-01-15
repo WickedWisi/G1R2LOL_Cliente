da4a60a18b3fc436fcad86734f42736ba6a8785c
josu <2dam@HZ375750.TartangaLH.eus>
2024-01-15 13:23:45 +0100

    CONTROLADOR SE VEN DATOS EN LA TABLA + cositas

--- src/controller/EventoController.java
+++ src/controller/EventoController.java
@@ -8,13 +8,19 @@ package controller;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableCell;
@@ -24,6 +30,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javax.ws.rs.core.GenericType;
 import logic.EventoManagerFactory;
 import model.Evento;
@@ -79,8 +86,8 @@ public class EventoController {
     private TableColumn tbcFechaEvento;
 
     public void initStage(Parent root) {
-        
-         LOGGER.info("HOLA HOLA HOLA HOLA");
+
+        LOGGER.info("INIT STAGE CLASE CONTROLADORA DE EVENTO");
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
@@ -111,12 +118,21 @@ public class EventoController {
         //La ventana no es redimensionable
         stage.setResizable(false);
 
-        //METODOS
+        //METODOS 
+        btnInsertar.setOnAction(this::handleCrearButtonAction);
+        btnInforme.setOnAction(this::handleInformeButtonAction);
+        btnEliminar.setOnAction(this::handleEliminarButtonAction);
+        btnEditar.setOnAction(this::handleModificarButtonAction);
+        btnBuscar.setOnAction(this::handleBuscarButtonAction);
+        stage.setOnCloseRequest(this::handleExitButtonAction);
+        tbvEvento.getSelectionModel().selectedItemProperty().addListener(this::handleUsersTableSelectionChanged);
+        //Con el siguiente codigo asignamos a las columnas los tipos y los nombres 
         tbcNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
         tbcDescripcion.setCellValueFactory(new PropertyValueFactory<>("descripcion"));
         tbcAforo.setCellValueFactory(new PropertyValueFactory<>("aforo"));
         tbcCatering.setCellValueFactory(new PropertyValueFactory<>("catering"));
         tbcFechaEvento.setCellValueFactory(new PropertyValueFactory<>("fechaEvento"));
+        //Aqui le ponemos un formato de fecha concreto 
         tbcFechaEvento.setCellFactory(column -> {
             TableCell<Evento, Date> cell = new TableCell<Evento, Date>() {
                 private SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
@@ -138,14 +154,70 @@ public class EventoController {
             return cell;
         });
 
-
-
+        //con las dos siguientes lineas agregamos a la tabla los datos 
         eventoData = FXCollections.observableArrayList(eventofact.getFactory().viewEvents_XML(Evento.class));
-        
+
         tbvEvento.setItems(eventoData);
 
         stage.show();
-       
+
+    }
+
+    @FXML
+    private void handleExitButtonAction(WindowEvent event) {
+        Alert ventanita = new Alert(Alert.AlertType.CONFIRMATION);
+        ventanita.setHeaderText(null);
+        ventanita.setTitle("Advertencia");
+        ventanita.setContentText("¿Deseas Salir?");
+        //Con este Optional<ButtonType> creamos botones de Ok y cancelar
+        Optional<ButtonType> action = ventanita.showAndWait();
+        //Si le da a OK el programa cesará de existir, se cierra por completo
+        if (action.get() == ButtonType.OK) {
+            Platform.exit();
+        } else {
+            //Si le da a cancelar la ventana emergente se cerrará pero la ventana principal se mantiene
+            ventanita.close();
+        }
+    }
+
+    @FXML
+    private void handleCrearButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleInformeButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleEliminarButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleModificarButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleBuscarButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleUsersTableSelectionChanged(ObservableValue observable, Object oldValue, Object newValue) {
+
+        if (newValue != null) {
+
+            Evento evento = (Evento) newValue;
+            tfNombre.setText(evento.getNombre());
+            taDescripcion.setText(evento.getDescripcion());
+            tfAforo.setText(evento.getAforo().toString());
+           // cbCatering.getse.select(lugar.getTipoLugar());
+           // dteTiempoReservado.setValue(lugar.getTiempo().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+        }
+
     }
 
     public void setStage(Stage stage) {
